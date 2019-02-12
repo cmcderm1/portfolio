455634317f9cf145945dc5e2b0bf72273c6391b3
cmcderm1 <44779832+cmcderm1@users.noreply.github.com>
2019-02-12 02:12:06 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
-if($_POST["message"]) {
-    mail("candice.marie.mcdermott@gmail.com", "Contact form submission.", $_POST["message"], "From: an@email.address");
-}
-?>
 
+if($_POST["submit"]) {
+    $recipient="candice.marie.mcdermott@gmail.com";
+    $subject="Contact Me form submission.";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! I will get back to you shortly.</p>";
+}
 
 
 <!DOCTYPE html>
